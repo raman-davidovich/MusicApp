ad0c4081382200632caec5925e74af2ede4a1be0
Raman Davidovich <raman.davidovich@gmail.com>
2024-01-07 17:32:21 +0300

    feat: implemented playing function

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,16 +1,25 @@
 import style from "./track.module.scss";
 import { IconButton } from "@mui/material";
-import { PlayArrow } from "@mui/icons-material";
+import { PlayArrow, Pause } from "@mui/icons-material";
 import secondsToMMSS from "../../utils/secondsToMMSS";
+import { useContext } from "react";
+import { AudioContext } from "../../context/AudioContex";
+import cn from "classnames";
 
 const Track = (track) => {
   const { id, src, preview, title, artists, duration } = track;
+
+  const { handleToggleAudio, currentTrack, isPlaying } =
+    useContext(AudioContext);
+
+  const isCurrentTrack = currentTrack.id === track.id;
+
   const formattedDuration = secondsToMMSS(duration);
 
   return (
-    <div className={style.track}>
-      <IconButton>
-        <PlayArrow />
+    <div className={cn(style.track, isCurrentTrack && style.playing)}>
+      <IconButton onClick={() => handleToggleAudio(track)}>
+        {isCurrentTrack && isPlaying ? <Pause /> : <PlayArrow />}
       </IconButton>
       <img
         className={style.preview}

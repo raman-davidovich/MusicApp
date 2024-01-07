68a1acec4ee9cfda68230217bd00c97385cc77a9
Raman Davidovich <raman.davidovich@gmail.com>
2024-01-07 13:12:00 +0300

    init: changed App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-const App = () => <>Music App</>;
+import MainPage from "./pages/MainPage/MainPage";
+import style from "./global.module.scss";
+
+const App = () => (
+  <div className={style.wrapper}>
+    <MainPage />
+  </div>
+);
 
 export default App;

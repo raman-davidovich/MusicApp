4bb64f0777a24e5f00d8c452880a537a74b74d9d
Raman Davidovich <raman.davidovich@gmail.com>
2024-01-07 14:26:21 +0300

    feat: added MainPage component structure

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,18 @@
-const MainPage = () => <>Main page</>;
+import tracksList from "../../assets/tracksList";
+import style from "./mainPage.module.scss";
+import Track from "../../components/Track/Track";
+
+const MainPage = () => {
+  return (
+    <div className={style.search}>
+      <>Search tracks</>
+      <div className={style.list}>
+        {tracksList.map((track) => (
+          <Track key={track.id} {...track} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;

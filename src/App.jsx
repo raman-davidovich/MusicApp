6e01cae959214f6b64cfa144528c05552e289790
Raman Davidovich <raman.davidovich@gmail.com>
2024-01-09 14:12:49 +0300

    feat: implemented Playbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import MainPage from "./pages/MainPage/MainPage";
 import style from "./global.module.scss";
+import Playbar from "./components/Playbar/Playbar";
 
 const App = () => (
   <div className={style.wrapper}>
     <MainPage />
+    <Playbar />
   </div>
 );
 
